1c6929505afa571ca4de8eeeda28142a46f5849f
mtdx <ms2asdk@gmail.com>
2017-08-25 06:18:32 -0700

    setup the database

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/mtdx/case-api/common"
 	"github.com/mtdx/case-api/middleware"
 )
 
@@ -9,6 +10,9 @@ func main() {
 	router := gin.Default()
 
 	router.Use(middleware.CORSMiddleware())
+
+	db := common.Connect("main")
+	defer db.Close()
 
 	apiv1 := router.Group("/api/v1")
 	{
